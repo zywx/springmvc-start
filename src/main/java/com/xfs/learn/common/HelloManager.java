4a94a1bab4054ad2bdd2aa850410fade153af6c4
wangzhongyou <wangzhongyou@baidu.com>
2018-02-10 18:49:04 +0800

    modify config

---
+++ src/main/java/com/xfs/learn/common/HelloManager.java
@@ -0,0 +1,10 @@
+package com.xfs.learn.common;
+
+/**
+ * A simple class desc
+ * <p>
+ * author fsnail.wang@gmail.com
+ * date 2018/2/10 下午6:46
+ */
+public class HelloManager {
+}
---
+++ src/main/java/com/xfs/learn/enums/Error.java
@@ -0,0 +1,10 @@
+package com.xfs.learn.enums;
+
+/**
+ * A simple class desc
+ * <p>
+ * author fsnail.wang@gmail.com
+ * date 2018/2/10 下午6:46
+ */
+public enum Error {
+}
